23f33ee4f3c5ab4a6d04bfddf198bc16ea30abdc
vishalgautamm <vishal.g@live.ca>
2016-11-03 12:47:00 -0400

    Add nav search support

--- app/components/weather/Weather.jsx
+++ app/components/weather/Weather.jsx
@@ -15,7 +15,9 @@ var Weather = React.createClass({
 
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         OpenWeatherMap.getTemp(location).then(function(temp) {
@@ -32,8 +34,27 @@ var Weather = React.createClass({
                 });
             });
     },
+
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+
+    componentWillReceiveProps: function (newProps) {
+        var location = newProps.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+
     render: function() {
-        var {isLoading, location,temp, errorMessage} = this.state;
+        var {isLoading, location, temp, errorMessage} = this.state;
         
         function renderMessage() {
             if (isLoading) {
